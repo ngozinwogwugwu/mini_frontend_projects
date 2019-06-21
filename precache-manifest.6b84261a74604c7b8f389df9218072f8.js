7e86459ace96f3f683f480a1b7c6546b7c4c5dfc
Ngozi Nwogwugwu <ngozi.n.nwogwugwu@gmail.com>
2019-06-21 16:26:19 -0400

    Updates

---
+++ precache-manifest.6b84261a74604c7b8f389df9218072f8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cd71d295a4a5763e78e1cc2acdcfb0",
+    "url": "/mini_frontend_projects/index.html"
+  },
+  {
+    "revision": "2d635990d1ac1d148f6c",
+    "url": "/mini_frontend_projects/static/css/main.9541d1c2.chunk.css"
+  },
+  {
+    "revision": "f833fa5814da88d6bfcc",
+    "url": "/mini_frontend_projects/static/js/2.0527072e.chunk.js"
+  },
+  {
+    "revision": "2d635990d1ac1d148f6c",
+    "url": "/mini_frontend_projects/static/js/main.510701ab.chunk.js"
+  },
+  {
+    "revision": "937ebd55d648b963ff54",
+    "url": "/mini_frontend_projects/static/js/runtime~main.6aa86fdd.js"
+  },
+  {
+    "revision": "40da0df060036015b56d07597c1eea0e",
+    "url": "/mini_frontend_projects/static/media/coffee.40da0df0.jpg"
+  },
+  {
+    "revision": "adef62251f9705f0373c9ab3d0a249c9",
+    "url": "/mini_frontend_projects/static/media/espresso.adef6225.jpg"
+  },
+  {
+    "revision": "5e9ab4bc7d6f3598bd4df90fe176ce6a",
+    "url": "/mini_frontend_projects/static/media/herbal_tea.5e9ab4bc.jpg"
+  },
+  {
+    "revision": "473041213d24347a3e4fbd7ea58532a9",
+    "url": "/mini_frontend_projects/static/media/latte.47304121.jpg"
+  },
+  {
+    "revision": "14351c735cc20ab102ab577f38c8460c",
+    "url": "/mini_frontend_projects/static/media/mocha.14351c73.jpg"
+  },
+  {
+    "revision": "98497820085ffe296fbe9966b2dcfb72",
+    "url": "/mini_frontend_projects/static/media/motorcycle-solid.98497820.svg"
+  }
+]);
